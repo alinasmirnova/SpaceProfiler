05c2911be6285ce0c0f34e5cd150a5d358d54df4
Alina Smirnova <BlueOstrich@yandex.ru>
2023-09-04 21:11:39 +0200

    Simple draft to order files by actual size
    (cherry picked from commit 7d877fbf5a2b4dbdc511a7477e3356756d02dd35)

--- SpaceProfilerConsole/Program.cs
+++ SpaceProfilerConsole/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using SpaceProfilerLogic;
+
+var directory = Console.ReadLine();
+
+var profiler = new Profiler();
+var result = profiler.GetOrderedFiles(directory);
+
+foreach (var fileInfo in result)
+{
+    Console.WriteLine($"{fileInfo.Length} {fileInfo.FullName}");
+}
---
+++ SpaceProfilerLogic/Profiler.cs
@@ -0,0 +1,30 @@
+﻿namespace SpaceProfilerLogic;
+
+public class Profiler
+{
+    public FileInfo[] GetOrderedFiles(string? directoryPath)
+    {
+        if (!Directory.Exists(directoryPath))
+            throw new ArgumentException("Directory does not exist");
+        
+        var result = new List<FileInfo>();
+
+        var queue = new Queue<string>();
+        queue.Enqueue(directoryPath);
+
+        while (queue.TryDequeue(out var curDirectory))
+        {
+            foreach (var file in Directory.EnumerateFiles(curDirectory))
+            {
+                result.Add(new FileInfo(file));
+            }
+
+            foreach (var directory in Directory.EnumerateDirectories(curDirectory))
+            {
+                queue.Enqueue(directory);
+            }
+        }
+        
+        return result.OrderByDescending(f => f.Length).ToArray();
+    }
+}
