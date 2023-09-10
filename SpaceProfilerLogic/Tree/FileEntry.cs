ed4906be122fcd7cb489eb4165b5d73cf632ef58
Alina Smirnova <BlueOstrich@yandex.ru>
2023-09-10 18:01:40 +0200

    Removed unused constructor
    (cherry picked from commit 2520a54f61bb1b426137b75225551f0d61d34eff)

--- SpaceProfilerLogic/Tree/FileEntry.cs
+++ SpaceProfilerLogic/Tree/FileEntry.cs
@@ -2,10 +2,6 @@
 
 public class FileEntry : FileSystemEntry
 {
-    public FileEntry(string fullName, FileSystemEntry? parent = null) : base(fullName, parent)
-    {
-    }
-    
     public FileEntry(string fullName, long size) : base(fullName)
     {
         Size = size;
